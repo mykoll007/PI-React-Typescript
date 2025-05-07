82c0ad0958c6659e3c1633ecac9c0785fde6b676
mykoll007 <mykoll.daniel@gmail.com>
2025-05-06 21:10:14 -0300

    first commit

---
+++ lista-campeões/src/Champs.tsx
@@ -0,0 +1,67 @@
+import "./assets/champs.css";
+import { useEffect, useState } from "react";
+
+interface Champion {
+  id: string;
+  name: string;
+  title: string;
+  image: {
+    full: string;
+  };
+}
+
+function Champs() {
+  const [champions, setChampions] = useState<Champion[]>([]);
+
+  useEffect(() => {
+    const fetchChamps = async () => {
+      try {
+        const res = await fetch(
+          "https://ddragon.leagueoflegends.com/cdn/14.9.1/data/pt_BR/champion.json"
+        );
+        const data = await res.json();
+        const champsArray = Object.values(data.data) as Champion[];
+        setChampions(champsArray);
+      } catch (err) {
+        console.error("Erro ao buscar campeões:", err);
+      }
+    };
+
+    fetchChamps();
+  }, []);
+
+  return (
+    <div className="container-app">
+      <div className="align-arrow">
+        <a href="/"><img src="/arrow-right.png" alt="seta-esquerda" /></a>
+      </div>
+
+      <div id="align-logoChamps">
+        <img src="/Logo.png" alt="logo" />
+      </div>
+
+      <h2 id="h2-champs">Campeões LoL</h2>
+
+      <div className="grid-view">
+      {champions.map((champ) => (
+  <div key={champ.id} className="cards-champ">
+    <img
+      src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champ.id}_0.jpg`}
+      alt={champ.name}
+      className="champions-img"
+    />
+    <p className="desc-nome">{champ.name}</p>
+    <p className="desc-titul">{champ.title}</p>
+  </div>
+))}
+
+      </div>
+
+      <div id="back-to-top">
+        <img src="/arrow-right.png" alt="seta-cima" />
+      </div>
+    </div>
+  );
+}
+
+export default Champs;
---
+++ lista-campeões/src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Champs from "./Champs"; // ajuste se usar React Router
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Champs />
+  </React.StrictMode>
+);
